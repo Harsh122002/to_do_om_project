6d2f76e10d131b42fac5d2ea8828267587cc7455
Harsh122002.git <chavadaharsh22@gmail.com.git>
2024-02-27 09:27:25 +0530

    s
    s

--- list.php
+++ list.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>To-Do List</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
